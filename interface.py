de3716e63746066d2ab80476d069f6821d4da465
Lyn <38800761+slynj@users.noreply.github.com>
2022-05-19 12:29:58 -0400

    Updated the Add student button
    - made it appear on the class menu
    - when hovered, colour changes

--- interface.py
+++ interface.py
@@ -6,8 +6,8 @@ import shutil
 import os
 
 
+# Copies the selected image file and saves it to the 'img' directory for the detector
 def fileCheck():
-    """Copies the selected image file and saves it to the 'img' directory for the detector"""
     dstPath = "img/"
     path = fd.askopenfilename(initialdir="/", title="Select file", filetypes=(("jpeg files", "*.jpeg"),
                                                                               ("png files", "*.png"),
@@ -18,59 +18,7 @@ def fileCheck():
         print(f'Something went wrong: {e}')
 
 
-def distFromPoints(point1, point2):
-    """Calculates the distance between two points"""
-    distance = math.sqrt(((point2[0] - point1[0]) ** 2) + ((point2[1] - point1[1]) ** 2))
-    return distance
-
-
-def createText(t, f="Arial", s=200, c=(255, 255, 0), b=False, i=False):
-    '''
-    Receives a string and other characters of a text and returns a rendered text
-    Parameters
-    ----------
-    t: str
-        the string of the text to be rendered
-    f: str = "Arial"
-        the string indicating the font style of the text
-    s: int = 200,
-        size of the text
-    c: tuple[int, int, int] = (255, 255, 0)
-        colour of the text
-    b: bool = False
-        indication if the text would be bold
-    i: bool = False
-        indication if the text would be italicized
-    Returns
-    -------
-    pygame.Surface
-        rendered text
-    '''
-
-    font = pygame.font.SysFont(f, s, bold=b, italic=i)
-    text = font.render(t, True, c)
-    return text
-
-
 def createBttn(mainSurface, text, textX, textY, c=(0, 0, 0)):
-    '''
-    Receives a text of the button with XY coordinates and draws a rectangle button with that text
-    Parameters
-    ----------
-    mainSurface: pygame.Surface
-        the surface to draw the elements
-    text: pygame.Surface
-        the rendered text of the button
-    textX: float
-        the X coordinate of the button
-    textY: float
-        the Y coordinate of the button
-    c: tuple[int, int, int] = (0, 0, 0)
-        colour of the button
-    Returns
-    -------
-    None
-    '''
     paddingW = text.get_width() * 0.3
     paddingH = text.get_height() * 0.1
     dimension = [textX - paddingW / 2, textY - paddingH / 2, text.get_width() + paddingW, text.get_height() + paddingH]
@@ -78,21 +26,8 @@ def createBttn(mainSurface, text, textX, textY, c=(0, 0, 0)):
     mainSurface.blit(text, (textX, textY))
 
 
-
+# Calculates what coordinate of the item's horizontal centre is
 def horizontalC(item, mainSurface):
-    '''
-    Calculates what coordinate of the item's horizontal centre is
-    Parameters
-    ----------
-    mainSurface: pygame.Surface
-        the surface to draw the elements
-    item: pygame.Surface
-        element to be horizontally centered
-    Returns
-    -------
-    int
-        the x coordinate where the element would be centered
-    '''
     return int((mainSurface.get_width() - item.get_width()) // 2)
 
 
@@ -104,7 +39,7 @@ def resizeImg(file, factor):
     return resizedImg
 
 
-def font(t, f="tommy.otf", s=40, c=(58, 101, 139)):
+def createText(t, f="tommy.otf", s=40, c=(58, 101, 139)):
     text = pygame.font.Font(f'resource/{f}', s)
     renderedText = text.render(t, True, c)
     return renderedText
@@ -118,13 +53,6 @@ def hoverObject(mouse, objects, objectX, objectY):
                  objects.get_width() + paddingW, objects.get_height() + paddingH]
     area = pygame.Rect(dimension)
     if area.collidepoint(mouse[0], mouse[1]):
-        return True
-    else:
-        return False
-
-
-def clickObject(objects, mouseUp):
-    if hoverObject(objects) and mouseUp:
         return True
     else:
         return False
@@ -158,6 +86,8 @@ def main():
     NAVY = (20, 61, 89)  # 143D59
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
+    GRAY = (230, 232, 237)
+    DARKGRAY = (213, 215, 219)
 
     programState = "MAIN"
 
@@ -179,9 +109,13 @@ def main():
 
     # Texts Init
     # Menu Header Texts
-    menuLogText = font('Log')
-    menuAttendanceText = font('Attendance')
-    menuClassText = font('Class')
+    menuLogText = createText('Log')
+    menuAttendanceText = createText('Attendance')
+    menuClassText = createText('Class')
+
+    # Add Student Button
+    addBttn = createText('Add', s=30, c=WHITE)
+    addBttnC = GRAY
 
     # -----------------------------Main Game Loop---------------------------------------- #
 
@@ -190,28 +124,14 @@ def main():
         # change order, make hover function
         mousePos = pygame.mouse.get_pos()
 
-        if distFromPoints(circlePos, mousePos) < circleSize:
-            hover = True
-            circleColor = (100, 0, 0)
-        else:
-            hover = False
-            circleColor = (255, 0, 0)
-
         ev = pygame.event.poll()
 
         if ev.type == pygame.QUIT:
             break
         if ev.type == pygame.MOUSEBUTTONUP:
             mouseUp = True
-            if hover:
-                circleColor = (255, 0, 0)
-                buttonOn = True
         else:
             mouseUp = False
-
-        if buttonOn:
-            buttonOn = False
-            fileCheck()
 
         # ----------------------------- Game Logic / Drawing -------------------------------- #
 
@@ -237,25 +157,36 @@ def main():
 
         # Menu Text Hover
         if hoverObject(mousePos, menuLogText, 350, 10):
-            menuLogText = font('Log', c=NAVY)
+            menuLogText = createText('Log', c=NAVY)
             if mouseUp:
                 programState = 'LOG'
         else:
-            menuLogText = font('Log', c=BLUE)
+            menuLogText = createText('Log', c=BLUE)
 
         if hoverObject(mousePos, menuAttendanceText, 550, 10):
-            menuAttendanceText = font('Attendance', c=NAVY)
+            menuAttendanceText = createText('Attendance', c=NAVY)
             if mouseUp:
                 programState = 'ATTENDANCE'
         else:
-            menuAttendanceText = font('Attendance', c=BLUE)
+            menuAttendanceText = createText('Attendance', c=BLUE)
 
         if hoverObject(mousePos, menuClassText, 900, 10):
-            menuClassText = font('Class', c=NAVY)
+            menuClassText = createText('Class', c=NAVY)
             if mouseUp:
                 programState = 'CLASS'
         else:
-            menuClassText = font('Class', c=BLUE)
+            menuClassText = createText('Class', c=BLUE)
+
+        # ——————— CLASS MENU ——————— #
+        if programState == 'CLASS':
+            createBttn(mainSurface, addBttn, 100, 100, addBttnC)
+            addBttnHover = hoverObject(mousePos, addBttn, 100, 100)
+            if addBttnHover:
+                addBttnC = DARKGRAY
+                if mouseUp:
+                    fileCheck()
+            else:
+                addBttnC = GRAY
 
 
         '''
