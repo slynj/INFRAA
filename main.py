ab6b3c30ea646fb8553fc8c15be360a536ddcbd8
Lyn <38800761+slynj@users.noreply.github.com>
2022-06-02 05:54:32 -0400

    Commenting

--- main.py
+++ main.py
@@ -8,14 +8,44 @@
 #
 # Author:      Lyn Jeong
 # Created:     05-May-2022
-# Updated:
+# Updated:     02-Jun-2022
 # -----------------------------------------------------------------------------
-# I think this project deserves a level XXXXXX because ...
+# I think this project deserves a level 4+ because it demonstrates all of the level
+# 4 criteria along with many extra features and implementations to improve the program.
+# INFRAA also implements a real world problem / situation and applies the knowledge learned
+# in the course to connect it back to my own interests and ideas.
 #
 # Features Added:
-#   ...
-#   ...
-#   ...
+#   - facial detection
+#       -> face detection based program
+#       -> detects the face, records the time and presence in a file
+#       -> on the detecting screen, a box is drawn with names to specify who was detected
+#   - threading
+#       -> runs two scripts in parallel to have easier / better user experience
+#       -> efficiently divides jobs for each scripts
+#   - file manipulations
+#       -> dynamically read/write/store data to display the information
+#   - adding new student
+#       -> new students can be added to the attendance program with just one picture
+#   - dynamic buttons / collision detection
+#       -> all the buttons react when they are hovered and are interactive
+#   - detailed help menus
+#       -> very detailed and user friendly help menus and instructions
+#   - animations
+#       -> moving main screen (changing images)
+#       -> moving loading screen
+#   - page controls
+#       -> page controlled by buttons and keyboards
+#       -> page number also shown
+#   - user friendly interface
+#       -> colour themed and layouted to be simple and eye catching
+#       -> appealing looks
+#   - clear program states
+#       -> various states dividing out different screens
+#   - effective use of loops
+#       -> effectively used for loops especially for displaying the images
+#   - created custom functions
+#       -> created many custom functions that are used throughout the program
 # -----------------------------------------------------------------------------
 from threading import Thread
 import subprocess
